dfc4ab100c55df86072f1abab47e4d2e3d19b8c9
RhysProbyn <rhysprobyn@gmail.com>
2022-08-08 22:15:33 +1000

    e1.3 commit

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,17 +1,24 @@
 // Header component
 const Header = (props) => {
-  return (
-    <p>
-      <h1>{props.headerText}</h1>
-    </p>
-  );
+  return <h1>{props.headerText}</h1>;
 };
 
 // Content component
 const Content = (props) => {
   return (
+    <div>
+      <Part name={props.part1} numberOfExercises={props.exercises1} />
+      <Part name={props.part2} numberOfExercises={props.exercises2} />
+      <Part name={props.part3} numberOfExercises={props.exercises3} />
+    </div>
+  );
+};
+
+// Part component
+const Part = (props) => {
+  return (
     <p>
-      {props.part} {props.numberOfExercises}
+      {props.name} {props.numberOfExercises}
     </p>
   );
 };
@@ -23,20 +30,31 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header headerText={course} />
-      <Content part={part1} numberOfExercises={exercises1} />
-      <Content part={part2} numberOfExercises={exercises2} />
-      <Content part={part3} numberOfExercises={exercises3} />
-      <Total numArray={[exercises1, exercises2, exercises3]} />
+      <Content
+        part1={part1.name}
+        part2={part2.name}
+        part3={part3.name}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+      />
+      <Total numArray={[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   );
 };
