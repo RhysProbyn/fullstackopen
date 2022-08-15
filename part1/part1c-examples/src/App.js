30e24925f1d5eb54efa7825acb47b5d917c884aa
RhysProbyn <rhysprobyn@gmail.com>
2022-08-15 23:25:16 +1000

    renamed p0 file

--- part1/part1c-examples/src/App.js
+++ part1/part1c-examples/src/App.js
@@ -5,26 +5,33 @@ const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
+
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
     setLeft(left + 1);
   };
 
-  const History = ({ allClicks }) => {
-    if (allClicks.length === 0) {
-      return <div>the app is used by pressing the buttons</div>;
-    }
-    return <div>button press history: {allClicks.join(" ")}</div>;
-  };
   const handleRightClick = () => {
     setAll(allClicks.concat("R"));
     setRight(right + 1);
   };
+
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return <div>the app is used by pressing the buttons</div>;
+    }
+    return <div>button press history: {props.allClicks.join(" ")}</div>;
+  };
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text}</button>
+  );
+
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
       {right}
       <History allClicks={allClicks} />
     </div>
