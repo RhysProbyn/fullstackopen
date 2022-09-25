2d3c529d73d7c65dea70c9c927da4f76b67e59a4
Rhys Probyn <rhys@Yoga-Dark.localdomain>
2022-09-25 16:24:56 +1000

    part2 exercises in-progress

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    if (persons.map((x) => x.name.toLowerCase()).includes(newName)) {
+      handleDuplicate(newName);
+    } else {
+      const nameObject = { name: newName };
+      setPersons(persons.concat(nameObject));
+    }
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handleDuplicate = (name) => {
+    window.alert(`${name} is already in the phonebook`);
+  };
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <Person key={person.name} name={person.name} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
